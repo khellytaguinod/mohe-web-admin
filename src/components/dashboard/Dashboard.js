ea2bec2b05e270acf7beb88f7b539e4937b91a5b
Khelly Taguinod <khelly.taguinod@98labs.com>
2020-04-24 22:31:23 +0800

    change some rerouting in web app

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -8,19 +8,23 @@ import { Redirect } from "react-router-dom";
 
 class Dashboard extends Component {
   render() {
-    const { projects, auth, notifications } = this.props;
+    const { projects, auth, notifications, userType } = this.props;
     if (!auth.uid) return <Redirect to="/sign-in" />;
+
+    if(userType === 'applicant') return <Redirect to="/application-submit-success" />;
+
+    if(userType === 'mohe') return <Redirect to="/dashboard" />;
 
     return (
       <div className="dashboard container">
-        <div className="row">
+        {/* <div className="row">
           <div className="col s12 m6">
             <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications notifications={notifications} />
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
@@ -31,7 +35,8 @@ const mapStateToProps = state => {
   return {
     projects: state.firestore.ordered.projects,
     auth: state.firebase.auth,
-    notifications: state.firestore.ordered.notifications
+    notifications: state.firestore.ordered.notifications,
+    userType: state.firebase.profile.userType
   };
 };
 
